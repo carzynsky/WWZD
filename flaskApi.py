8b765b5206173beb48b36762ce6585b804483318
AREK CARZYNSKI <carzynski1@gmail.com>
2021-11-22 23:53:58 +0100

    Add rp get route and  not working version of umap

--- flaskApi.py
+++ flaskApi.py
@@ -1,18 +1,25 @@
 import json
-from numpy.core.fromnumeric import shape
+from numpy.core.fromnumeric import shape, transpose
 import pandas as pd
 import numpy as np
 from flask import Flask, json
 from sklearn.decomposition import PCA
+import umap
 import statistics
 
 # file paths
 filePathRpHerbertKgr10Data = 'data/rp_herbert-kgr10.json'
 filePathRp = 'data/rp.json'
 
-json_df = pd.read_json(filePathRp, encoding='UTF-8')
-
 api = Flask(__name__)
+
+@api.route('/rp', methods=['GET'])
+def get_rp():
+    jsonDf = json_df.to_json(force_ascii=False)
+    return json.dumps(
+    {
+        "data": jsonDf
+    })
 
 @api.route('/pca', methods=['GET'])
 def get_results_of_pca():
@@ -38,10 +45,6 @@ def get_hello():
 
 def pca():
     # open and load file
-    print('Load rpHerbertKgr10 data')
-    f = open(filePathRpHerbertKgr10Data)
-    rpHerbertKgr10Data_df = json.load(f)
-    A = rpHerbertKgr10Data_df['data']
     print('Set pca components')
     pca = PCA(n_components=128)
     print('Started fitting...')
@@ -51,6 +54,24 @@ def pca():
     A_2 = pca.transform(A)
     print(A_2.shape)
 
+# def umap():
+#     global B
+#     B = umap.UMAP(n_neighbors=5,
+#                       min_dist=0.3,
+#                       metric='correlation').fit_transform(A)
+#     print(type(B))
+
+def preload():
+    print('Load rpHerbertKgr10 data')
+    f = open(filePathRpHerbertKgr10Data)
+    rpHerbertKgr10Data_df = json.load(f)
+    global A
+    A = rpHerbertKgr10Data_df['data']
+    global json_df
+    json_df = pd.read_json(filePathRp, encoding='utf-8')
+
 if __name__ == '__main__':
+    preload()
     pca()
+    #umap()
     api.run() 
